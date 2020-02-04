4302596fd837e114c2fa834428713787d7ba5a40
Jens-Andreas Rensaa <jens.rensaa@gmail.com>
2020-02-04 16:12:09 +0100

    Add provider service skeleton with web3j codegen

---
+++ providerService/src/main/java/xyz/rensaa/providerservice/ProviderserviceApplication.java
@@ -0,0 +1,13 @@
+package xyz.rensaa.providerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderserviceApplication.class, args);
+	}
+
+}
---
+++ providerService/src/test/java/xyz/rensaa/providerservice/ProviderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.rensaa.providerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
