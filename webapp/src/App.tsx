ac6c9447412de534c55f976411292b572a02fc2c
Jens-Andreas Rensaa <jens.rensaa@gmail.com>
2020-02-26 11:15:34 +0100

    webapp: Add react router boilerplate

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,34 +1,63 @@
 import React, { useContext } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
 import WebSocketContext from "./hooks/useWebSocket";
 
-const App = () => {
-  const stomp = useContext(WebSocketContext);
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-  stomp.watch("/authority/proposal").subscribe(message => {
-    console.log(message.body);
-  });
+/*
+const stomp = useContext(WebSocketContext);
 
+stomp.watch("/authority/proposal").subscribe(message => {
+  console.log(message.body);
+});
+
+*/
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default App;
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
---
+++ webapp/src/properties.ts
@@ -0,0 +1,5 @@
+const config = {
+  contract_file: process.env.REACT_APP_CONTRACT_FILE || "../contracts/shared/"
+};
+
+export default config;
