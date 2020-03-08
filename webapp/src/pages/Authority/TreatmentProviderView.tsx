6036d192965126d03cce3783d73750c0800467ac
Jens-Andreas Rensaa <jens.rensaa@gmail.com>
2020-03-08 17:58:01 +0100

    Add initial treatmentProviderView

--- webapp/src/pages/Authority/TreatmentProviderView.tsx
+++ webapp/src/pages/Authority/TreatmentProviderView.tsx
@@ -1,10 +1,111 @@
-import React, { useContext } from "react";
-import TreatmentProviderContext from "../TreatmentProvider/context/TreatmentProviderContext";
+import React, {
+  useContext,
+  useState,
+  useEffect,
+  FunctionComponent
+} from "react";
+import TreatmentProviderMessage from "../../dto/TreatmentProvider";
+import KeyContext from "../../context/KeyContext";
+import TreatmentProviderViewContext from "../../context/TreatmentProviderViewContext";
+import styled from "styled-components";
+import { Card, HTMLTable } from "@blueprintjs/core";
+
+const AreaGrid = styled.div`
+  display: grid;
+  grid-template-rows: 80px auto auto auto;
+  grid-template-columns: 500px auto;
+  grid-template-areas:
+    "title none"
+    "send send"
+    "keystitle keystitle"
+    "keys keys";
+`;
+
+const CardAreaWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  width: 100%;
+  grid-area: keys;
+`;
+
+const CardWrapper = styled.div`
+  margin: 10px;
+`;
+
+const WrappingTableData = styled.td`
+  font-size: 12px;
+  vertical-align: bottom !important;
+  word-break: break-all;
+`;
+
+const TableWrapper = styled.div`
+  height: 150px;
+  width: 410px;
+`;
+
+interface ProviderCardProps {
+  provider: TreatmentProviderMessage;
+}
+
+const ProviderCard: FunctionComponent<ProviderCardProps> = ({ provider }) => {
+  return (
+    <CardWrapper>
+      <Card elevation={1}>
+        <TableWrapper>
+          <HTMLTable small>
+            <thead>
+              <tr>
+                <th>Address</th>
+                <td>{provider.address}</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>IsTrusted</th>
+                <td>{provider.isTrusted ? "true" : "false"}</td>
+              </tr>
+            </tbody>
+          </HTMLTable>
+        </TableWrapper>
+      </Card>
+    </CardWrapper>
+  );
+};
 
 const TreatmentProviderView = () => {
-  const { treatmentProvider } = useContext(TreatmentProviderContext);
+  const { treatmentProviders } = useContext(TreatmentProviderViewContext);
+  const { activeKey } = useContext(KeyContext);
 
-  return <h2>Treatment Provider View</h2>;
+  const trustedProviders: TreatmentProviderMessage[] = [];
+  let untrustedProviders: TreatmentProviderMessage[] = [];
+
+  if (activeKey) {
+    treatmentProviders.forEach(provider => {
+      if (provider?.trustedBy?.includes(activeKey.address)) {
+        treatmentProviders.push(provider);
+      } else {
+        untrustedProviders.push(provider);
+      }
+    });
+  } else {
+    untrustedProviders = treatmentProviders;
+  }
+
+  const trustedProviderCards = trustedProviders.map(provider => (
+    <ProviderCard key={provider.address} provider={provider} />
+  ));
+
+  const untrustedProviderCards = untrustedProviders.map(provider => (
+    <ProviderCard key={provider.address} provider={provider} />
+  ));
+
+  return (
+    <AreaGrid>
+      <h1>Treatment Provider trust management</h1>
+      <CardAreaWrapper>{untrustedProviderCards}</CardAreaWrapper>
+    </AreaGrid>
+  );
 };
 
 export default TreatmentProviderView;
